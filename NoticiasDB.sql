40fa61c9230e811d2184e14e306d332777a51deb
CerrutiGH <66427525+CerrutiGH@users.noreply.github.com>
2021-06-24 11:10:36 -0300

    Update NoticiasDB.sql

--- NoticiasDB.sql
+++ NoticiasDB.sql
@@ -1,8 +1,8 @@
-drop database if exists db_Melhores_Noticias;
-CREATE DATABASE db_Melhores_Noticias;
+DROP DATABASE IF EXISTS db_Melhores_Noticias;
+CREATE DATABASE IF NOT EXISTS db_Melhores_Noticias;
 USE db_Melhores_Noticias;
 
-CREATE TABLE tb_Noticias(
+CREATE TABLE IF NOT EXISTS tb_Noticias(
 COD_NOTICIA INT PRIMARY KEY AUTO_INCREMENT,
 TITULO_NOTICIA VARCHAR(100) NOT NULL,
 DESCRICAO_NOTICIA VARCHAR(100) NOT NULL,
@@ -16,12 +16,4 @@ VALUES("O time da escola ganha dos catados", "O time da escola vence por 10X2 de
 ("Custo da Escola", "O custo das escolas aumentaram", "Escola", str_to_date("01/01/0001", "%d/%m/%Y")),
 ("Diario de um Nerd", "Diario de um Nerd é o novo livro mais comentado", "Escola", str_to_date("01/01/0001", "%d/%m/%Y"));
 
-
-INSERT INTO tb_Noticias(TITULO_NOTICIA, DESCRICAO_NOTICIA, CATEGORIA_NOTICIA, DATA_NOTICIA)
-VALUES("Gabs", "Allowwwwwww", "Sas", str_to_date("01/01/0001", "%d/%m/%Y"));
-
 select * from tb_Noticias;
-
-SELECT * FROM tb_Noticias WHERE CATEGORIA_NOTICIA ="Esportes";
-
-SELECT * FROM tb_Noticias ORDER BY COD_NOTICIA DESC LIMIT 3 ;
